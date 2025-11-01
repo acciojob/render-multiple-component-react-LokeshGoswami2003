438ff901743898a13d7e0f591e1d1af4b369ff2d
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-11-01 12:37:49 +0530

    final

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ import App from "./components/App";
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App name='The alchemist'
+    description="Self-help Book" />, document.getElementById("root"));
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+const App = (props) => {
   return (
-    <div id="main"></div>
+    <div id="main" className="ns-wrapper">
+     <h1 data-ns-test="project-name">{props.name}</h1> 
+     <h6 data-ns-test="project-description"> {props.description}</h6>
+     </div>
   )
 }
 
